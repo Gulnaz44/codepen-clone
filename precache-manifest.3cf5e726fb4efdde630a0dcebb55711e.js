fa372b9501b9d3fb51b437c7d5471bc746c74dc5
Gulnaz Afsara <gulnazfara@gmail.com>
2023-01-03 22:08:59 +0530

    Updates

---
+++ precache-manifest.3cf5e726fb4efdde630a0dcebb55711e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58e75fcf5fa086976b1852ece28e3991",
+    "url": "/codepen-clone/index.html"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/codepen-clone/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "54c5845b4d697b1b065d",
+    "url": "/codepen-clone/static/css/main.7d2118c4.chunk.css"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/codepen-clone/static/js/2.4254f81d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/codepen-clone/static/js/2.4254f81d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54c5845b4d697b1b065d",
+    "url": "/codepen-clone/static/js/main.849b43c7.chunk.js"
+  },
+  {
+    "revision": "7e95b33d3c3f8b32c9c4",
+    "url": "/codepen-clone/static/js/runtime-main.3a6f25c4.js"
+  }
+]);
